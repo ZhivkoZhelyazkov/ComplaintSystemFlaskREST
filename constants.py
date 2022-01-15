c4f4d56097487b156a0bb284b058116dfce032f1
ZhivkoZhelyazkov <zkzhelyazkov@gmail.com>
2022-01-15 17:13:39 +0200

    Integrate s3 service

---
+++ constants.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# ROOT_DIR = os.path.dirname(os.path.join(os.path.abspath(__file__), '..'))   # In case this file is in a directory
+TEMP_FILE_FOLDER = os.path.join(ROOT_DIR, 'temp_files')
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_migrate import Migrate
 from psycopg2.errorcodes import UNIQUE_VIOLATION
 from werkzeug.exceptions import BadRequest, InternalServerError
-
+from flask_cors import CORS
 from config import DevApplication
 from db import db
 from resources.routes import routes
@@ -13,6 +13,7 @@ app.config.from_object(DevApplication)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+CORS(app)
 api = Api(app)
 
 [api.add_resource(*r) for r in routes]
@@ -32,3 +33,4 @@ def conclude_request(response):
 
 if __name__ == "__main__":
     app.run()
+    # app.run(host='127.0.0.1', port=8000, debug=True)
